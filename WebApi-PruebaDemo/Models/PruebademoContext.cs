cd2d0b814fc0372f6b8d07c3af98b3c37ade2011
CZubileteSanchez <carloszubilete@gmail.com>
2025-07-10 23:37:28 -0300

    We've changed the conection string from Context.cs to appsettings.json. Also we've assigned the coonection string to the context of the database into Program.cs

--- WebApi-PruebaDemo/Models/PruebademoContext.cs
+++ WebApi-PruebaDemo/Models/PruebademoContext.cs
@@ -6,102 +6,103 @@ namespace WebApi_PruebaDemo.Models;
 
 public partial class PruebademoContext : DbContext
 {
-    public PruebademoContext()
+  public PruebademoContext()
+  {
+  }
+
+  public PruebademoContext(DbContextOptions<PruebademoContext> options)
+      : base(options)
+  {
+  }
+
+  public virtual DbSet<Cliente> Clientes { get; set; }
+
+  public virtual DbSet<Producto> Productos { get; set; }
+
+  public virtual DbSet<Venta> Ventas { get; set; }
+
+  public virtual DbSet<Ventasitem> Ventasitems { get; set; }
+  
+  protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)// To defined our Connecting string
+  {
+    
+  }
+
+  protected override void OnModelCreating(ModelBuilder modelBuilder)
+  {
+    modelBuilder.Entity<Cliente>(entity =>
     {
-    }
+      entity.HasKey(e => e.Id).HasName("PK__clientes__3214EC2711F41545");
 
-    public PruebademoContext(DbContextOptions<PruebademoContext> options)
-        : base(options)
+      entity.ToTable("clientes");
+
+      entity.Property(e => e.Id).HasColumnName("ID");
+      entity.Property(e => e.Cliente1)
+              .HasMaxLength(255)
+              .IsUnicode(false)
+              .HasColumnName("Cliente");
+      entity.Property(e => e.Correo)
+              .HasMaxLength(255)
+              .IsUnicode(false);
+      entity.Property(e => e.Telefono)
+              .HasMaxLength(255)
+              .IsUnicode(false);
+    });
+
+    modelBuilder.Entity<Producto>(entity =>
     {
-    }
+      entity.HasKey(e => e.Id).HasName("PK__producto__3214EC27C9FC63BD");
 
-    public virtual DbSet<Cliente> Clientes { get; set; }
+      entity.ToTable("productos");
 
-    public virtual DbSet<Producto> Productos { get; set; }
+      entity.Property(e => e.Id).HasColumnName("ID");
+      entity.Property(e => e.Categoria)
+              .HasMaxLength(255)
+              .IsUnicode(false);
+      entity.Property(e => e.Nombre)
+              .HasMaxLength(255)
+              .IsUnicode(false);
+    });
 
-    public virtual DbSet<Venta> Ventas { get; set; }
-
-    public virtual DbSet<Ventasitem> Ventasitems { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-LFTFVP5\\SQLEXPRESS;Initial Catalog=pruebademo;Integrated Security=True;TrustServerCertificate=true");
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    modelBuilder.Entity<Venta>(entity =>
     {
-        modelBuilder.Entity<Cliente>(entity =>
-        {
-            entity.HasKey(e => e.Id).HasName("PK__clientes__3214EC2711F41545");
+      entity.HasKey(e => e.Id).HasName("PK__ventas__3214EC273EE29B0D");
 
-            entity.ToTable("clientes");
+      entity.ToTable("ventas");
 
-            entity.Property(e => e.Id).HasColumnName("ID");
-            entity.Property(e => e.Cliente1)
-                .HasMaxLength(255)
-                .IsUnicode(false)
-                .HasColumnName("Cliente");
-            entity.Property(e => e.Correo)
-                .HasMaxLength(255)
-                .IsUnicode(false);
-            entity.Property(e => e.Telefono)
-                .HasMaxLength(255)
-                .IsUnicode(false);
-        });
+      entity.Property(e => e.Id).HasColumnName("ID");
+      entity.Property(e => e.Fecha).HasColumnType("datetime");
+      entity.Property(e => e.Idcliente).HasColumnName("IDCliente");
 
-        modelBuilder.Entity<Producto>(entity =>
-        {
-            entity.HasKey(e => e.Id).HasName("PK__producto__3214EC27C9FC63BD");
+      entity.HasOne(d => d.IdclienteNavigation).WithMany(p => p.Venta)
+              .HasForeignKey(d => d.Idcliente)
+              .OnDelete(DeleteBehavior.ClientSetNull)
+              .HasConstraintName("FK_ventas_clientes");
+    });
 
-            entity.ToTable("productos");
+    modelBuilder.Entity<Ventasitem>(entity =>
+    {
+      entity.HasKey(e => e.Id).HasName("PK__ventasit__3214EC27D34D9564");
 
-            entity.Property(e => e.Id).HasColumnName("ID");
-            entity.Property(e => e.Categoria)
-                .HasMaxLength(255)
-                .IsUnicode(false);
-            entity.Property(e => e.Nombre)
-                .HasMaxLength(255)
-                .IsUnicode(false);
-        });
+      entity.ToTable("ventasitems");
 
-        modelBuilder.Entity<Venta>(entity =>
-        {
-            entity.HasKey(e => e.Id).HasName("PK__ventas__3214EC273EE29B0D");
+      entity.Property(e => e.Id).HasColumnName("ID");
+      entity.Property(e => e.Idproducto).HasColumnName("IDProducto");
+      entity.Property(e => e.Idventa).HasColumnName("IDVenta");
 
-            entity.ToTable("ventas");
+      entity.HasOne(d => d.IdproductoNavigation).WithMany(p => p.Ventasitems)
+              .HasForeignKey(d => d.Idproducto)
+              .OnDelete(DeleteBehavior.ClientSetNull)
+              .HasConstraintName("FK_ventasitems_productos");
 
-            entity.Property(e => e.Id).HasColumnName("ID");
-            entity.Property(e => e.Fecha).HasColumnType("datetime");
-            entity.Property(e => e.Idcliente).HasColumnName("IDCliente");
+      entity.HasOne(d => d.IdventaNavigation).WithMany(p => p.Ventasitems)
+              .HasForeignKey(d => d.Idventa)
+              .OnDelete(DeleteBehavior.ClientSetNull)
+              .HasConstraintName("FK_ventasitems_ventas");
+    });
 
-            entity.HasOne(d => d.IdclienteNavigation).WithMany(p => p.Venta)
-                .HasForeignKey(d => d.Idcliente)
-                .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK_ventas_clientes");
-        });
+    OnModelCreatingPartial(modelBuilder);
+  }
 
-        modelBuilder.Entity<Ventasitem>(entity =>
-        {
-            entity.HasKey(e => e.Id).HasName("PK__ventasit__3214EC27D34D9564");
-
-            entity.ToTable("ventasitems");
-
-            entity.Property(e => e.Id).HasColumnName("ID");
-            entity.Property(e => e.Idproducto).HasColumnName("IDProducto");
-            entity.Property(e => e.Idventa).HasColumnName("IDVenta");
-
-            entity.HasOne(d => d.IdproductoNavigation).WithMany(p => p.Ventasitems)
-                .HasForeignKey(d => d.Idproducto)
-                .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK_ventasitems_productos");
-
-            entity.HasOne(d => d.IdventaNavigation).WithMany(p => p.Ventasitems)
-                .HasForeignKey(d => d.Idventa)
-                .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK_ventasitems_ventas");
-        });
-
-        OnModelCreatingPartial(modelBuilder);
-    }
-
-    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+  partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
 }
--- WebApi-PruebaDemo/Program.cs
+++ WebApi-PruebaDemo/Program.cs
@@ -1,3 +1,6 @@
+using WebApi_PruebaDemo.Models; // include the file Models.
+using Microsoft.EntityFrameworkCore; // Include the library.
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// To add the connection string to the context of the Database.
+builder.Services.AddDbContext<PruebademoContext>(options =>
+{
+  options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionStringSQL"));
+});
 
 var app = builder.Build();
 
