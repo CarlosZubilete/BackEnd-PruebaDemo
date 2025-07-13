ed300d0d555a335e0ba40e52e79611233f93894b
CZubileteSanchez <carloszubilete@gmail.com>
2025-07-13 03:42:26 -0300

    add into program.cs app.UseCors('NewPolicy')

--- WebApi-PruebaDemo/Program.cs
+++ WebApi-PruebaDemo/Program.cs
@@ -34,7 +34,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseCors(); // Use the CORS Middlware.
+app.UseCors("NewPolicy"); // Use the CORS Middlware.
 
 app.UseAuthorization();
 
